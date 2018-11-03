58669158b62f6c7654ab46e667e1e0ed3ed2bc76
Timo Kluck <tkluck@infty.nl>
2018-11-03 16:58:53 +0100

    Mirror Frank Lübeck's data on Github, and use HTTP package
     - I suspect the HTTP package is more portable, particularly
    to minimal container environments.
    
    _- Hosting on Github means I don't worry so much about issues
    with the download, as at install time, you've likely just downloaded
    the build script from github as well. So let's remove the try/catch.
    That also seems to fix another (scoping?) issue that I didn't manage
    to reproduce.

--- deps/build.jl
+++ deps/build.jl
@@ -1,27 +1,21 @@
 using Serialization
+using HTTP
 using JSON
 
+
+url = "https://gist.githubusercontent.com/tkluck/e1cd1746c69aa17e4a37114d22649627/raw/7fbe9763fae27f14924262ad03606f1c3af4400e/CPImport.txt"
 ResDataType = Dict{Tuple{Int,Int}, Vector{Int}}
 result = ResDataType()
 
-try
-    data = read(`curl -L 'http://www.math.rwth-aachen.de/~Frank.Luebeck/data/ConwayPol/CPimport.txt'`)
-    data = transcode(String, data)
+data = String(HTTP.request("GET", url).body)
+data = replace(data, r"allConwayPolynomials := " => "")
+data = replace(data, r"];"                       => "]")
 
-    data = replace(data, r"allConwayPolynomials := " => "")
-    data = replace(data, r"];"                       => "]")
+list = JSON.parse(data)
+# Frank Lübeck's data format signifies the end of the array with a null marker
+pop!(list)
 
-    list = JSON.parse(data)
-    # Frank Lübeck's data format signifies the end of the array with a null marker
-    pop!(list)
-
-    result = ResDataType((p,n) => coeffs for (p,n,coeffs) in list)
-catch exception
-    @warn """
-        Issue downloading Conway polynomials database: $exception
-        Please retry the build step again later.
-    """
-end
+result = ResDataType((p,n) => coeffs for (p,n,coeffs) in list)
 
 open("conwaypolynomials.data", "w") do f
     serialize(f, result)
