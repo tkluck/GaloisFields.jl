d3bde346abc406fefa9278d6bbe1e83e05c49cdb
Timo Kluck <tkluck@infty.nl>
2019-12-17 16:14:19 +0100

    CI: pre-compile the module in the runtests.jl script
    I'm seeing failures on Apple+Windows related to world-age and also
    warnings related to pre-compiling. My guess is that pre-compiling
    is not race-condition-free and so I should avoid doing it in two
    spawned processes simultaneously. If my hypothesis is correct,
    this commit fixes the build status.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
+using GaloisFields # make sure pre-compilation happens in a single process
+
 tasks = mapreduce(&, ["using-bitintegers.jl", "not-using-bitintegers.jl"]) do script
     fullpath = joinpath(@__DIR__, script)
     `$(Base.julia_cmd()) $fullpath`
